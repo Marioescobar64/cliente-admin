992ac4e4e145bbb30f4c72efa69cd1473853167f
JosueChan-2022047 <mescobar-2022471@kinal.edu.gt>
2026-04-20 08:35:46 -0600

    Hola

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import './../styles/index.css'
 import App from './App.jsx'
 
+// console.log("Comprobando Variables", import.meta.env.VITE_AUTH_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
